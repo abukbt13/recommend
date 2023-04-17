fe387fb1d3563b9ed43d79dd0d717e59a6e12d58
abukbt13 <abukbt13@gmail.com>
2023-04-18 01:18:42 +0300

    edit

--- src/more/edit_company.vue
+++ src/more/edit_company.vue
@@ -1,23 +1,17 @@
 <template>
     <div class="d-flex align-content-center justify-content-center">
-        <div class=" w-50" v-for="company in companies" :key="company">
-            <h2>Edit Company Details</h2>
+        <div class=" w-50" >
+            <h2>Edit Company </h2>
             <form @submit.prevent="submit">
                 <div class="form-group my-2">
                     <label for="">Company name</label>
-                    <input type="text" class="form-control" v-model="company.company_name"  placeholder="Enter The Company">
+                    <input type="text" class="form-control" v-model="state.company_name"  placeholder="Enter The Company">
                 </div>
                 <div class="form-group my-2">
                     <label for="">Company Url</label>
-                    <input type="text" class="form-control" v-model="company.url"  placeholder="Enter The Company URL">
+                    <input type="text" class="form-control" v-model="state.url"  placeholder="Enter The Company URL">
                 </div>
-                <div class="form-group my-2">
-                    <label for="">Image</label>
-                    <div class="card-body">
-                        <img :src="'http://127.0.0.1:8000/storage/company/'+company.company_logo" style="width:60%; height: 14rem;">
-                    </div>
-                    <input type="file" class="form-control"  @change="onFileChange"   placeholder="Enter The Company">
-                </div>
+
 
                 <button type="submit" class="btn btn-primary my-2 px-4">Update Company Details</button>
             </form>
@@ -28,47 +22,29 @@
 
 <script setup>
 import axios from 'axios'
-import {computed, onMounted, ref} from 'vue'
+import {computed, onMounted, reactive} from 'vue'
 import {useRoute} from "vue-router";
-import async from "async";
-// const message=ref('');
 
 const route = useRoute();
-const companies = ref([]);
-const id = computed(() => route.query.id);
-const company_name = ref('');
-const url = ref('');
-const company_logo = ref(null);
 
-const onFileChange=(e)=>{
-    new_image.value=e.target.files[0];
-}
-const company = async () => {
+const state = reactive({
+    company_name: '',
+    url: '',
+});
+
+const id = computed(() => route.query.id);
+
+const getCompany = async () => {
     const res = await axios.get(`http://127.0.0.1:8000/api/edit_company/${id.value}`);
     if (res.status == 200) {
-        companies.value = res.data;
-        company_name.value = res.data.company_name;
-        url.value = res.data.url;
-    }
-};
-
-const submit= async () =>{
-    if(!title.value){
-        alert('Please select a title')
-    }
-    else {
-        const formData=new FormData();
-        formData.append('title',title.value);
-        formData.append('image',image.value);
-        formData.append('new_image',new_image.value);
-        const res = await axios.post(`http://127.0.0.1:8000/api/update_image/${route.params.id}`,formData)
-        if(res.status==200){
-            alert('successfully updated')
-            getImage()
-        }
+        console.log(res.data)
+        state.company_name = res.data.company_name;
+        state.url = res.data.url;
     }
 }
+
 onMounted(() => {
-    company();
+    getCompany();
 });
 </script>
+
